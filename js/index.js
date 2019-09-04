3551e9ee1d3dc437f720fc0eace0bf0dbb854cf3
Dee Barizo <deebarizo@gmail.com>
2019-09-04 03:17:03 -0500

    Add event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Your code goes here
+// 1
+let contactLink = document.querySelector("nav a:nth-child(4)");
+
+contactLink.addEventListener("click", () => {
+  alert("Email us at info@funbus.com.");
+});
+
+// 2, 3
+let mainNavigation = document.querySelector(".main-navigation");
+
+mainNavigation.addEventListener("mouseenter", event => {
+  event.target.style.backgroundColor = "#E9EEF1";
+});
+
+mainNavigation.addEventListener("mouseleave", event => {
+  event.target.style.backgroundColor = "white";
+});
+
+// 4, 5
+addEventListener("keydown", () => {
+  let headerImage = document.querySelector("header.intro img");
+  headerImage.style.display = "none";
+});
+
+addEventListener("keyup", () => {
+  let headerImage = document.querySelector("header.intro img");
+  headerImage.style.display = "inline";
+});
